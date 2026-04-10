7d52900c07ce3fa073531dd6b76cc2e059809503
Henderson <hendersondike@gmail.com>
2026-04-10 10:28:39 +0100

    added facebook metadata to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,14 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <script defer src="https://cloud.umami.is/script.js" data-website-id="60187f63-0781-44b0-9c80-2f13523f0cf6"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','925243956941129');fbq('track','PageView');`,
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=925243956941129&ev=PageView&noscript=1" alt="" />
+        </noscript>
       </head>
       <body
         className={`${dmSans.variable} ${geistMono.variable} antialiased`}
